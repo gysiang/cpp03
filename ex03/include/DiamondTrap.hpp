3fdc47733c5aae72f4fbd2208f8d0e474092b021
gysiang <gysiang@gmail.com>
2024-10-26 12:52:05 +0800

    added DiamondTrap

---
+++ ex03/include/DiamondTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 12:48:16 by gyong-si          #+#    #+#             */
+/*   Updated: 2024/10/26 12:51:10 by gyong-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMOND_TRAP_HPP
+# define DIAMOND_TRAP_HPP
+
+#include "ClapTrap.hpp"
+#include "ScravTrap.hpp"
+#include "FragTrap.hpp"
+
+class Diamond_Trap
+{
+	protected:
+		std::string _name;
+
+	private:
+};
+
+#endif
---
+++ ex03/src/FragTrap.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 00:19:48 by gyong-si          #+#    #+#             */
+/*   Updated: 2024/10/26 12:35:48 by gyong-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap() {
+	this->_hitPoint = 100;
+	this->_energyPoint = 100;
+	this->_attackDamage = 30;
+	std::cout << "FragTrap Default Constructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string name) : ClapTrap(name) {
+	this->_hitPoint = 100;
+	this->_energyPoint = 100;
+	this->_attackDamage = 30;
+	std::cout << "FragTrap Constructor for " << this->_name << " called" << std::endl;
+}
+
+FragTrap::~FragTrap() {
+	std::cout << "FragTrap Deconstructor called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &copy) : ClapTrap(copy) {
+	std::cout << "FragTrap Copy Constructor called" << std::endl;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &a) {
+	std::cout << "FragTrap Assignation operator called" << std::endl;
+	this->_name = a._name;
+	this->_hitPoint = a._hitPoint;
+	this->_energyPoint = a._energyPoint;
+	this->_attackDamage = a._attackDamage;
+	return (*this);
+}
+
+void	FragTrap::highFivesGuys(void) {
+	std::cout << "\033[0;31mFragTrap " << this->_name << ": Gives you a high five!\033[0m " << std::endl;
+};
